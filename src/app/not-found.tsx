1b513e79b4f9cc42b7e594e2ae22d83a04cc0650
streafy <streafyhs@gmail.com>
2024-12-18 04:00:33 +0700

    add not found error page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import styles from './not-found.module.css';
+
+const NotFound = () => {
+  return (
+    <main className={styles.container}>
+      <h1>Страница не найдена</h1>
+    </main>
+  );
+};
+
+export default NotFound;
