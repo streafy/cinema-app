8dbe259b6d40790487a10e2e1e60015f84f989d4
streafy <streafyhs@gmail.com>
2024-12-18 04:02:25 +0700

    add incorrect movie id handling on movie details page

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -1,5 +1,6 @@
 import MovieDetails from '@/components/movie-details/movie-details';
 import { fetchMovieDetails } from '@/services/fetchMovieDetails';
+import { notFound } from 'next/navigation';
 
 import styles from './page.module.css';
 
@@ -11,6 +12,11 @@ type MovieDetailsPageProps = {
 
 const MovieDetailsPage = async ({ params }: MovieDetailsPageProps) => {
   const movieId = parseInt((await params).id);
+
+  if (isNaN(movieId)) {
+    return notFound();
+  }
+
   const movie = await fetchMovieDetails(movieId);
 
   return (
