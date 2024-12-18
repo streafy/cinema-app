f4c2f6d6a04f88b83911a3cac6530c0b842d4480
streafy <streafyhs@gmail.com>
2024-12-18 22:45:33 +0700

    add error page

---
+++ src/app/error.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import styles from './error.module.css';
+
+const Error = () => {
+  return (
+    <main className={styles.container}>
+      <h1>Произошла ошибка при загрузке данных</h1>
+    </main>
+  );
+};
+
+export default Error;
